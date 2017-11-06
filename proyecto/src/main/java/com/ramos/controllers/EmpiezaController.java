36aea1603355c1678269b1dc1878a55b8c92821f
Rodrigo <exit>
2017-11-06 15:30:37 -0500

    Primer commit

--- proyecto/src/main/java/com/ramos/controllers/EmpiezaController.java
+++ proyecto/src/main/java/com/ramos/controllers/EmpiezaController.java
@@ -3,7 +3,7 @@ package com.ramos.controllers;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-
+//Hola
 @Controller
 public class EmpiezaController {
 	@RequestMapping(value="/empieza",method = RequestMethod.GET)
